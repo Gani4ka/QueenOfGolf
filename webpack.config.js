5f1256308d0da46091b4f0fe05924a9fd4705538
Anya Efimova <gani4ka_n@ukr.net>
2019-03-11 00:05:20 +0200

    Ñnew plagin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
@@ -67,7 +68,8 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "styles.css"
-    })
+    }),
+    new CopyWebpackPlugin([{ from: "src/img", to: "img" }])
   ],
   devServer: {
     historyApiFallback: true,
